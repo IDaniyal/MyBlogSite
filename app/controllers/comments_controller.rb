4695776039cab20057e9677e81be32db02273140
IDaniyal <daniyal.akhlaq@sendoso.com>
2020-10-20 12:33:46 +0500

    Few changes in comment functionality

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,7 @@
 class CommentsController < ApplicationController
   before_action :set_comment, only: [:show, :edit, :update, :destroy]
+  before_action :set_blog, only: [:create, :destroy]
+
 
 
   def index
@@ -17,7 +19,6 @@ class CommentsController < ApplicationController
   end
 
   def create
-    @blog = Blog.find(params[:blog_id])
     unless @blog.nil?
       @user = current_user
       @user.comments.create!(blog: @blog,  text: comment_params[:text])
@@ -42,8 +43,7 @@ class CommentsController < ApplicationController
   def destroy
     @comment.destroy
     respond_to do |format|
-      format.html { redirect_to comments_url, notice: 'Comment was successfully destroyed.' }
-      format.json { head :no_content }
+      format.html { redirect_to blog_path(params[:blog_id]), notice: 'Comment was successfully destroyed.' }
     end
   end
 
@@ -51,6 +51,10 @@ class CommentsController < ApplicationController
     def set_comment
       @comment = Comment.find(params[:id])
     end
+
+  def set_blog
+    @blog = Blog.find(params[:blog_id])
+  end
 
     def comment_params
       params.require(:comment).permit(:text)
